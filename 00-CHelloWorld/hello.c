ab7a846c2f824ab04e6756db9f56552880f6d1b7
ketzeLitvak <elitvak@frba.utn.edu.ar>
2022-04-04 19:35:51 -0300

    changes to 00-CHelloWorld

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -2,8 +2,8 @@
 
 int main() {
   FILE *fhello_world;
-  fhello_world = fopen("helloWorld.txt", "w");
-  fprintf(fhello_world,"Hello World");
+  fhello_world = fopen("output.txt", "w");
+  fprintf(fhello_world,"Hello World! :D");
   fclose(fhello_world);
   return 0;
 }
