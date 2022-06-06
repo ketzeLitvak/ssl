fdaeb8abd3fad0c4c00fc45bee4864e5092ee270
ketzeLitvak <elitvak@frba.utn.edu.ar>
2022-06-06 00:34:40 -0300

    02-FasesErrores

---
+++ pruebas/ejClase/hello4.c
@@ -0,0 +1,6 @@
+int printf(const char * restrict s, ...); // Declaración
+// Declaración + definición
+int main(void){
+int i=42; // Declaración y definición
+ prontf("La respuesta es %d\n"); // Sentencia
+}
