5546ebb1ce8fe40abfc1f3f0f41c050404370fb9
ketzeLitvak <elitvak@frba.utn.edu.ar>
2022-06-06 22:54:43 -0300

    arreglos hablado en clase + parte de tp 02

--- 01-Interfaces/TablasDeConversion.c
+++ 01-Interfaces/TablasDeConversion.c
@@ -5,13 +5,25 @@
 */
 #include <stdio.h>
 #include "Conversion.h"
-#include "TablasDeConversion.h"
 
+// Declaraciones
+void PrintFila(int value, double (*func)(double));
+void PrintFilas(double (*func)(double), const char* col1, const char* col2, int base, int limit, int step);
+void PrintTablaCelsius(int base, int limit, int step);
+void PrintTablaFahrenheit(int base, int limit, int step);
+
+int main(void) {
+    PrintTablaFahrenheit(0, 300, 20);
+    PrintTablaCelsius(0, 300, 20);
+    return 0;
+}
+
+// Definiciones
 void PrintFila(int value, double (*func)(double)) {
     printf("|%4d | %6.2f |\n", value, func(value));
 }
 
-void PrintFilas(double (*func)(double), char* col1, char* col2, int base, int limit, int step) {
+void PrintFilas(double (*func)(double), const char* col1, const char* col2, int base, int limit, int step) {
     printf(" --------------\n");
     printf("|%4s | %6s |\n", col1, col2);
     printf("|--------------|\n");
@@ -27,10 +39,4 @@ void PrintTablaCelsius(int base, int limit, int step) {
 
 void PrintTablaFahrenheit(int base, int limit, int step) {
     PrintFilas(Celsius, "fahr", "cel", base, limit, step);
-}
-
-int main(void) {
-    PrintTablaFahrenheit(0, 300, 20);
-    PrintTablaCelsius(0, 300, 20);
-    return 0;
 }
--- 02-FasesErrores/hello6.c
+++ 02-FasesErrores/hello6.c
@@ -1,6 +1,6 @@
 int printf(const char * restrict s, ...);
 
 int main(void){
-int i=42;
- prontf("La respuesta es %d\n");
+    int i=42;
+    printf("La respuesta es %d\n", i);
 }
