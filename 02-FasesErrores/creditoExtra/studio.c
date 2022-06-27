c4149d1824208ec0df4f07f23937f7fb85cb9a98
ketzeLitvak <elitvak@frba.utn.edu.ar>
2022-06-26 23:38:06 -0300

    Entrega tp 2

---
+++ 02-FasesErrores/creditoExtra/studio.c
@@ -0,0 +1,9 @@
+#include "studio.h" // Interfaz que exporta
+#include <stdio.h> // Interfaz que importa
+void prontf(const char* s, int i){
+ printf("La respuesta es %d\n", i);
+}
+
+int multiplyBy2(int i){
+    return i * 2;
+}
